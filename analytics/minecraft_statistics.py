2d037c86f526a63be3a216c61dc9dd86300d630a
imgVOID <imgvoid@gmail.com>
2022-02-19 16:23:53 +0200

    refactor

--- analytics/minecraft_statistics.py
+++ analytics/minecraft_statistics.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv("history_activity/02.2022/Day 19.csv", index_col='id')
-df["login"] = df["login"].str.replace(r"\\", "")
-df["ads_source"] = df["ads_source"].str.replace(r"\\", "")
+df["login"] = df["login"].str.replace("\\", "")
+df["ads_source"] = df["ads_source"].str.replace("\\", "")
 df["launched_at"] = pd.to_datetime(df['launched_at'])
 df = df[(df['launched_at'] > "2018-01-01")]
 print(df)
--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from routers.minecraft_statistics import router_minecraft_statistics
 from scheduled.minecraft_statistics import save_statistics_dump
 from config import config
 
-app = FastAPI()
+
+app = FastAPI(docs_url='/api/minecraft/docs', redoc_url='/api/minecraft/redoc')
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -14,12 +15,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(router_minecraft_statistics)
+app.include_router(router_minecraft_statistics, prefix="/api/minecraft/statistics")
 
 
 @app.on_event('startup')
 def init_data():
-    scheduler = AsyncIOScheduler()
+    scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
     scheduler.add_job(
         save_statistics_dump, config.SCHEDULER_TIMINGS["statistics_dump"]["trigger"],
         **config.SCHEDULER_TIMINGS["statistics_dump"]["datetime"]
@@ -28,4 +29,4 @@ def init_data():
 
 
 if __name__ == '__main__':
-    uvicorn.run(app="main:app", host="127.0.0.1", port=8080, reload=True, debug=False)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8080, reload=True, debug=False, log_level="info")
